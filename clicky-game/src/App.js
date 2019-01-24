364bd02e1a926b637b89e5c1887fd697af6ce3ce
BettyWop <annettekalla1@gmail.com>
2019-01-23 18:48:27 -0800

    updated components in src forms

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import cards from "./cards.json";
+import Scoreboard from "./components/Scoreboard";
+import Card from "./components/Card";
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    randomId: 1,
+    score: 0,
+    tally: 0,
+    characters: cards
+  };
+
+  componentDidMount() {
+    this.setNewRandomId(this.state.characters);
+  }
+  
   render() {
     return (
      <div>
@@ -12,3 +27,4 @@ class App extends Component {
 }
 
 export default App;
+ 
