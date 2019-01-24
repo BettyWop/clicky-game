244af85c9a2d5240ae18c406d0e2f6d94e1d041f
BettyWop <annettekalla1@gmail.com>
2019-01-23 20:36:55 -0800

    got images to load

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -7,24 +7,59 @@ import './App.css';
 class App extends Component {
 
   state = {
-    randomId: 1,
+    result: 1,
     score: 0,
-    tally: 0,
+    topScore: 0,
     characters: cards
   };
 
   componentDidMount() {
     this.setNewRandomId(this.state.characters);
   }
-  
+
+  handleClicked = id => {
+    this.setState({
+      topScore: this.state.topScore + 1
+    });
+    if (this.state.result === id) {
+      this.setState({
+        score: this.state.score + 1
+      });
+      this.setNewRandomId(this.state.characters);
+    }
+  }
+
+  setNewRandomId = (array) => {
+    const result = array[Math.floor(Math.random() * array.length)].id;
+    this.setState({
+      result: result
+    });
+  }
+
   render() {
     return (
-     <div>
-
+      <div className="container">
+        <Scoreboard
+          title="Clicky Game Woot-Woot"
+          score={this.state.score}
+          topScore={this.state.topScore}
+          result={this.state.result}
+        />
+        <div className="row">
+          {this.state.characters.map(characters => (
+            <Card
+              key={characters.id}
+              id={characters.id}
+              name={characters.name}
+              image={characters.image}
+              handleClicked={this.handleClicked}
+            />
+          ))}
      </div>
-    );
-  }
-}
-
-export default App;
+     </div>
+        );
+      }
+    }
+    
+    export default App;
  
--- clicky-game/src/components/Scoreboard/index.js
+++ clicky-game/src/components/Scoreboard/index.js
@@ -2,16 +2,12 @@ import React from "react";
 
 function Scoreboard(props) {
     return (
-        <header>
-            <div className="row">
-                <div className="col-md-12">
-                    <h1>{props.title}</h1>
-                    <p>Random Number: {props.randomId}</p>
-                    <p>Score: {props.score}</p>
-                    <p>Tally: {props.tally}</p>
-                </div>
-            </div>
-        </header>
+        <nav className="nav">
+            <a class="nav-link active" href="#"><strong>Clicky Game</strong></a>
+            <a class="nav-link" href="#">Click an image to begin!</a>
+            <a class="nav-item">{props.result}</a>
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Score: {props.score} | Top Score: {props.topScore}</a>
+        </nav>
     )
 }
 
