d2f747ebcc23a0aecf16a0f222ade336c607a2a3
BettyWop <annettekalla1@gmail.com>
2019-01-28 14:09:39 -0800

    updated file push

---
+++ src/component/Score/index.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import './score.css'
+
+function Score(props){
+    return (
+    <ul class="nav row scoregame">
+        <li className="nav-item col-md-3">
+        <a href="#" className="nav-link">Clicky Game</a>
+        </li>
+        <li className="nav-item col-md-3">
+        <a href="#" className="nav-link">{props.result}</a>
+        </li>
+        <li className="nav-item col-md-3">
+        <a href="#" className="nav-link">Score: {props.score} | Top Score: {props.topScore}</a>
+        </li>
+    </ul>
+        )
+}
+
+export default Score;
